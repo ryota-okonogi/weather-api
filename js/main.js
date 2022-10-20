5479466506b9f8b64495f6557baf52020c868309
nogi5000 <R5000.power@gmail.com>
2022-10-20 17:36:24 +0900

    break

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
-// 'use strict';
-
 // const url = '【API URL】';
 const url = 'https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&daily=temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo';
 
 
-fetch(url)
-.then(data => data.json())
-.then(json => console.log(json))
-.then(json => drawChart(json)) // 追加 //指摘箇所
+fetch(url) // URLにアクセス
+.then(data => data.json()) // レスポンスをdataに格納
+.then(json => drawChart(json)) // 追加
 
 function drawChart(json) {
+  console.log(json) // 変更(記述箇所を修正)
   const mydata = {
-    labels: json.daily.time, // 指摘箇所
+    labels: json.daily.time,
     datasets: [{
       label: '最高気温',
       data: json.daily.temperature_2m_max,
@@ -28,5 +26,3 @@ function drawChart(json) {
     data: mydata,
   });
 }
-
-// hourly
